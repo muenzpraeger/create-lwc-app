71469afa3a73cdc89a4b34d15e2a1a5adde71ec2
muenzpraeger <mail@winkelmeyer.com>
2019-05-31 16:26:22 -0700

    Message updates

--- packages/lwc-services/src/messages/watch.ts
+++ packages/lwc-services/src/messages/watch.ts
@@ -2,8 +2,8 @@ export const messages = {
     description: 'Runs a Lightning Web Components project in watch mode',
     help: {
         examples: [
-            'lwc-services serve',
-            'lwc-services serve -p 3998 -h 192.168.178.12 -m production'
+            'lwc-services watch',
+            'lwc-services watch -p 3998 -h 192.168.178.12 -m production'
         ]
     },
     errors: {
@@ -20,7 +20,7 @@ export const messages = {
         open: 'opens the site in the default browser',
         mode: 'defines the mode for the build (production|development)',
         host: 'sets the hostname',
-        port: 'configures the port of the application',
+        port: 'configures the port of the server',
         webpack: 'location of custom webpack configuration file'
     },
     logs: {

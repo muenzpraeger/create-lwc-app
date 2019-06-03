27105f04bad8d83a44af741f28b30a370f802736
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-06-03 11:12:06 +0200

    Restructure templates for client/server option (#16)

---
+++ packages/lwc-create-app/templates/lwc-services.client.js
@@ -0,0 +1,5 @@
+// Find the full example of all available configuration options at
+// https://github.com/muenzpraeger/lwc-create-app/blob/master/packages/lwc-services/example/lwc-services.config.js
+module.exports = {
+    resources: [{ from: 'src/resources', to: 'dist/resources' }]
+};

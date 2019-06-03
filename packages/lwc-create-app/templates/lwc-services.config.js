8951e0110d76091c56f05fb6a0d5491e7d806afc
muenzpraeger <mail@winkelmeyer.com>
2019-06-03 17:32:14 +0200

    Add `moduleDir` to lwc-services.config.js

--- packages/lwc-create-app/templates/lwc-services.config.js
+++ packages/lwc-create-app/templates/lwc-services.config.js
@@ -1,8 +1,9 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/lwc-create-app/blob/master/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/client/resources', to: 'dist/resources' }],<% if (clientserver) { %>
+    resources: [{ from: 'src<% if (clientserver) { %>/client<% } %>/resources', to: 'dist/resources' }],<% if (clientserver) { %>
     sourceDir: './src/client',
+    moduleDir: './src/client/modules',
     server: {
         customConfig: './src/server/index.js'
     }<% } %>

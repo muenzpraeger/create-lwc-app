d6250bfe8d2d099cc4e4f355d8b0f10e7f2eab70
muenzpraeger <mail@winkelmeyer.com>
2019-06-03 11:11:41 +0200

    Restructure templates for client/server option

---
+++ packages/lwc-create-app/templates/lwc-services.server.js
@@ -0,0 +1,8 @@
+// Find the full example of all available configuration options at
+// https://github.com/muenzpraeger/lwc-create-app/blob/master/packages/lwc-services/example/lwc-services.config.js
+module.exports = {
+    resources: [{ from: 'src/client/resources', to: 'dist/resources' }],
+    server: {
+        customConfig: './src/server/index.js'
+    }
+};
---
+++ packages/lwc-create-app/templates/src/server/index.js
@@ -0,0 +1,3 @@
+module.exports = app => {
+    // put your express app logic here
+};

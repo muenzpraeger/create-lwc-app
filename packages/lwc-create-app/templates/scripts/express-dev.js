f29756e60837ede38d3dc2c3b0204fa94e488293
muenzpraeger <mail@winkelmeyer.com>
2019-09-02 12:19:50 +0200

    Express server additions

---
+++ packages/lwc-create-app/templates/scripts/express-dev.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+<% if (typescript) { %>const customServer = require(path.resolve('./lib/server/')).default;<% } else { %>const customServer = require(path.resolve('./src/server/'));<% } %>
+
+const app = express();
+
+customServer(app);
+
+app.listen(3002, () => {
+    // eslint-disable-next-line no-console
+    console.log('Yay, local server started');
+});

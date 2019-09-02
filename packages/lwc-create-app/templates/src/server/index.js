eec8cecf95bf6afa7aac6086a8152f32b1fd4ef7
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-09-02 12:50:52 +0200

    Express server additions (#76)

--- packages/lwc-create-app/templates/src/server/index.js
+++ packages/lwc-create-app/templates/src/server/index.js
@@ -1,3 +1,8 @@
+// eslint-disable-next-line no-undef
 module.exports = app => {
     // put your express app logic here
+    app.get('/some/api', (req, res) => {
+        // do stuff
+        res.json({ status: 'ok' });
+    });
 };

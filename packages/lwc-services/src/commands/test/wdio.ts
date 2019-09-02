58f79c7f9f0675ae713602d80bec5f152ea25f35
muenzpraeger <mail@winkelmeyer.com>
2019-09-02 16:18:39 +0200

    Adding wdio spawn

--- packages/lwc-services/src/commands/test/wdio.ts
+++ packages/lwc-services/src/commands/test/wdio.ts
@@ -1,7 +1,10 @@
 import { Command, flags } from '@oclif/command'
+import * as path from 'path'
 
 import { messages } from '../../messages/wdio'
 import { log, welcome } from '../../utils/logger'
+
+const spawn = require('child_process').spawn
 
 export default class Wdio extends Command {
     static description = messages.description
@@ -19,7 +22,18 @@ export default class Wdio extends Command {
 
         log(messages.logs.starting_wdio)
 
-        const wdio = require('@wdio/cli')
-        wdio.run('wdio.conf.js')
+        const wdioExecutable = path.resolve(
+            'node_modules/@wdio/cli/bin/wdio.js'
+        )
+        const wdioSpawn = spawn('node', [wdioExecutable])
+
+        wdioSpawn.on('error', (err: string) => {
+            log({ message: `${err}`, emoji: 'sos' })
+        })
+
+        // It's super weird that the debug message is passed via stderr. But it is what it is.
+        wdioSpawn.stderr.on('data', (data: string) => {
+            log({ message: `${data}`, emoji: 'rainbow' })
+        })
     }
 }

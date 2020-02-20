101f52d85514161829eb07e55c2c5e1f66789f8b
muenzpraeger <mail@winkelmeyer.com>
2020-02-20 12:33:54 +0100

    Minor fixes for first beta

--- scripts/test-publish.sh
+++ scripts/test-publish.sh
@@ -8,7 +8,9 @@ cd packages
 cd create-lwc-app
 rm -f tsconfig.tsbuildinfo
 tsc -b
-npm publish --registry=http://localhost:4873 --tag beta
+# npm publish --registry=http://localhost:4873 --tag beta
+npm publish --registry=https://registry.npmjs.org --tag beta
 cd ../lwc-services
 yarn build
-npm publish --registry=http://localhost:4873 --tag beta
+# npm publish --registry=http://localhost:4873 --tag beta
+npm publish --registry=https://registry.npmjs.org --tag beta

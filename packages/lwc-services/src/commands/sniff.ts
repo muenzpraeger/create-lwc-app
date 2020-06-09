73e089fb3cc44f2d764bbf3343d723c54bbbc44b
muenzpraeger <mail@winkelmeyer.com>
2020-06-09 06:24:54 +0200

    chore: linting updates

--- packages/lwc-services/src/commands/sniff.ts
+++ packages/lwc-services/src/commands/sniff.ts
@@ -30,6 +30,7 @@ export default class Sniff extends Command {
         })
     }
 
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     async run() {
         const { flags } = this.parse(Sniff)
 

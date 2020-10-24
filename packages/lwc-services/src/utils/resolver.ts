4cc0e66562603452a76a0005012d25fa12a294b5
muenzpraeger <mail@winkelmeyer.com>
2020-10-24 18:37:03 +0200

    feat: simplify Jest resolver

--- packages/lwc-services/src/utils/resolver.ts
+++ packages/lwc-services/src/utils/resolver.ts
@@ -1,73 +1,13 @@
-const fs = require('fs')
-const path = require('path')
 const lwcJestResolver = require('@lwc/jest-resolver')
 const lwcModuleResolver = require('@lwc/module-resolver')
-const { isValidModuleName, getInfoFromId } = require('./webpack/module')
-import { lwcConfig } from '../config/lwcConfig'
 
-function getProjectInfo(): {
-    modulesDir: string
-    namespaces: string
-} {
-    const cwd = fs.realpathSync(process.cwd())
-    const modulesDir = path.resolve(cwd, lwcConfig.sourceDir, 'modules')
-    const namespaces = fs.readdirSync(modulesDir)
-    return { modulesDir, namespaces }
-}
-
-function isFile(file: string): boolean {
-    let result = false
-
-    try {
-        const stat = fs.statSync(file)
-        result = stat.isFile() || stat.isFIFO()
-    } catch (e) {
-        if (!(e && e.code === 'ENOENT')) {
-            throw e
-        }
-        result = false
-    }
-
-    return result
-}
-
-function resolveAsFile(name: string, extensions: string[]): string | null {
-    if (isFile(name)) {
-        return name
-    }
-
-    for (const extension of extensions) {
-        const file = name + extension
-        if (isFile(file)) {
-            return file
-        }
-    }
-    return null
-}
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = function (modulePath: string, options: any): string {
-    if (isValidModuleName(modulePath)) {
-        const { modulesDir } = getProjectInfo()
-        const { ns, name } = getInfoFromId(modulePath)
-
-        try {
-            const mod = lwcModuleResolver.resolveModule(
-                modulePath,
-                process.cwd()
-            )
-            return mod.entry
-        } catch (e) {
-            // do nothing
-        }
-
-        const file = resolveAsFile(
-            path.join(modulesDir, ns, name, name),
-            options.extensions
-        )
-
-        if (file) {
-            return fs.realpathSync(file)
-        }
+    try {
+        const mod = lwcModuleResolver.resolveModule(modulePath, process.cwd())
+        return mod.entry
+    } catch (e) {
+        // do nothing
     }
     // eslint-disable-next-line prefer-rest-params
     return lwcJestResolver(...arguments)

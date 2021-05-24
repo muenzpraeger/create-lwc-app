d5dd728d6d951c3aef7971eae3a8568005323aa6
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2021-05-24 10:26:22 +0200

    feat: change `serve` to `start` scripts / PWA config update (#876)
    * feat: change `serve` to `start` scripts
    
    * fix: change default PWA config
    
    Fixes #832

--- packages/create-lwc-app/templates/lwc-services.config.js
+++ packages/create-lwc-app/templates/lwc-services.config.js
@@ -2,17 +2,14 @@
 // https://github.com/muenzpraeger/create-lwc-app/blob/main/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
     resources: [{ from: 'src<% if (clientserver) { %>/client<% } %>/resources/<% if (bundler === 'rollup') { %>**<% } %>', to: 'dist/resources/' }
-        <% if ((appType === 'pwa' || bundler==='rollup') && !clientserver) { %>
+        <% if (bundler==='rollup' && !clientserver) { %>
             , { from: 'src/index.html', to: 'dist/'}, { from: 'src/manifest.json', to: 'dist/'}
         <% } %>
         <% if (((appType === 'pwa' && bundler==='rollup') || bundler === 'rollup') && clientserver) { %>
             , { from: 'src/client/index.html', to: 'dist/'}
         <% } %>
-        <% if (appType === 'pwa' && bundler!=='rollup' && clientserver) { %>
-            , { from: 'src/client/index.html', to: 'dist/index.html'}
-        <% } %>
-        <% if (appType === 'pwa' && clientserver) { %>
-            , { from: 'src/client/manifest.json', to: 'dist/manifest.json'}
+        <% if (appType === 'pwa') { %>
+            , { from: 'src<% if (clientserver) { %>/client<% } %>/manifest.json', to: 'dist/manifest.json'}
         <% } %>
             ],
         <% if (clientserver) { %>
--- packages/create-lwc-app/templates/webpack.config.js
+++ packages/create-lwc-app/templates/webpack.config.js
@@ -1,7 +1,13 @@
 // Custom webpack configuration file, provides generation of service worker
 // More information: https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin
 const { GenerateSW } = require('workbox-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '..', 'dist'),
+        filename: '[name].js'
+    },
     plugins: [new GenerateSW({ swDest: 'sw.js' })]
 };
